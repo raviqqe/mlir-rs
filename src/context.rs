f8d6489a817fa18284b335f1c2064ea8eef69665
Yota Toyama <raviqqe@gmail.com>
2022-07-06 15:51:07 +0000

    Revert refactor

--- src/context.rs
+++ src/context.rs
@@ -1,5 +1,3 @@
-use mlir_sys::*;
-
 #[derive(Debug)]
 pub struct Context {
     context: mlir_sys::MlirContext,
@@ -9,14 +7,14 @@ impl Context {
     #[allow(clippy::new_without_default)]
     pub fn new() -> Self {
         Self {
-            context: unsafe { mlirContextCreate() },
+            context: unsafe { mlir_sys::mlirContextCreate() },
         }
     }
 }
 
 impl Drop for Context {
     fn drop(&mut self) {
-        unsafe { mlirContextDestroy(self.context) };
+        unsafe { mlir_sys::mlirContextDestroy(self.context) };
     }
 }
 
