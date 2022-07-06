267148b4a7b4a29a4df67ae76ee392fea5be196f
Yota Toyama <raviqqe@gmail.com>
2022-07-06 15:26:56 +0000

    Fix setup

--- tools/setup.sh
+++ tools/setup.sh
@@ -2,4 +2,11 @@
 
 set -e
 
-brew install llvm@14
+llvm_version=14
+
+brew update
+brew install llvm@$llvm_version
+
+if [ -n "$GITHUB_ENV" ]; then
+  echo PATH=$(brew --prefix)/opt/llvm@$llvm_version/bin:$PATH >>$GITHUB_ENV
+fi
