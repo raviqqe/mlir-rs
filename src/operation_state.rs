1c28168bd10018e3ef57c6b82a3e863e35f7701c
Yota Toyama <raviqqe@gmail.com>
2022-07-08 03:58:12 +0000

    Use raw operation state

--- src/operation_state.rs
+++ src/operation_state.rs
@@ -1,95 +1,52 @@
 use crate::{
     attribute::Attribute,
-    block::Block,
+    context::Context,
+    identifier::Identifier,
     location::Location,
-    r#type::Type,
-    region::Region,
     utility::{as_string_ref, into_raw_array},
-    value::Value,
 };
-use mlir_sys::{mlirIdentifierGet, mlirNamedAttributeGet, MlirOperationState};
-use std::collections::HashMap;
+use mlir_sys::{
+    mlirNamedAttributeGet, mlirOperationStateAddAttributes, mlirOperationStateGet,
+    MlirOperationState,
+};
+use std::marker::PhantomData;
 
 pub struct OperationState<'c> {
-    name: String,
-    location: Location<'c>,
-    results: Vec<Type<'c>>,
-    operands: Vec<Value>,
-    regions: Vec<Region>,
-    successors: Vec<Block>,
-    attributes: HashMap<String, Attribute<'c>>,
-    enable_result_type_inference: bool,
+    state: MlirOperationState,
+    _context: PhantomData<&'c Context>,
 }
 
 impl<'c> OperationState<'c> {
-    pub fn new(name: impl Into<String>, location: Location<'c>) -> Self {
+    pub fn new(name: impl AsRef<str>, location: Location<'c>) -> Self {
         Self {
-            name: name.into(),
-            location,
-            results: vec![],
-            operands: vec![],
-            regions: vec![],
-            successors: vec![],
-            attributes: Default::default(),
-            enable_result_type_inference: false,
+            state: unsafe {
+                mlirOperationStateGet(as_string_ref(name.as_ref()), location.to_raw())
+            },
+            _context: Default::default(),
         }
     }
 
-    pub fn set_attribute(&mut self, name: impl AsRef<str>, attribute: Attribute<'c>) {
-        self.attributes.insert(name.as_ref().into(), attribute);
-    }
-
-    pub(crate) fn into_raw(self) -> MlirOperationState {
+    pub fn set_attribute(&mut self, attributes: Vec<(Identifier, Attribute<'c>)>) -> &mut Self {
         unsafe {
-            MlirOperationState {
-                name: as_string_ref(&self.name),
-                location: self.location.to_raw(),
-                nResults: self.results.len() as isize,
-                results: into_raw_array(
-                    self.results
+            mlirOperationStateAddAttributes(
+                &mut self.state,
+                attributes.len() as isize,
+                into_raw_array(
+                    attributes
                         .into_iter()
-                        .map(|r#type| r#type.to_raw())
-                        .collect(),
-                ),
-                nOperands: self.operands.len() as isize,
-                operands: into_raw_array(
-                    self.operands
-                        .into_iter()
-                        .map(|value| value.to_raw())
-                        .collect(),
-                ),
-                nRegions: self.regions.len() as isize,
-                regions: into_raw_array(
-                    self.regions
-                        .into_iter()
-                        .map(|region| region.to_raw())
-                        .collect(),
-                ),
-                nSuccessors: self.successors.len() as isize,
-                successors: into_raw_array(
-                    self.successors
-                        .into_iter()
-                        .map(|block| block.to_raw())
-                        .collect(),
-                ),
-                nAttributes: self.attributes.len() as isize,
-                attributes: into_raw_array(
-                    self.attributes
-                        .into_iter()
-                        .map(|(name, attribute)| {
-                            mlirNamedAttributeGet(
-                                mlirIdentifierGet(
-                                    self.location.context().to_raw(),
-                                    as_string_ref(&name),
-                                ),
-                                attribute.to_raw(),
-                            )
+                        .map(|(identifier, attribute)| {
+                            mlirNamedAttributeGet(identifier.to_raw(), attribute.to_raw())
                         })
                         .collect(),
                 ),
-                enableResultTypeInference: self.enable_result_type_inference,
-            }
+            )
         }
+
+        self
+    }
+
+    pub(crate) unsafe fn into_raw(self) -> MlirOperationState {
+        self.state
     }
 }
 
@@ -104,7 +61,10 @@ mod tests {
         let context = Context::new();
         let mut state = OperationState::new("foo", Location::unknown(&context));
 
-        state.set_attribute("bar", Attribute::parse(&context, "unit"));
+        state.set_attribute(vec![(
+            Identifier::new(&context, "bar"),
+            Attribute::parse(&context, "unit"),
+        )]);
 
         Operation::new(state);
     }
