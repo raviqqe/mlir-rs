1ca4be1e47f58ae85e16822f80faf536a24fe3ad
Yota Toyama <raviqqe@gmail.com>
2022-09-10 20:41:46 +0000

    Update

--- src/string_ref.rs
+++ src/string_ref.rs
@@ -1,17 +1,24 @@
 use mlir_sys::{mlirStringRefCreateFromCString, MlirStringRef};
+use once_cell::sync::Lazy;
 use std::{
+    collections::HashMap,
     ffi::{CStr, CString},
     marker::PhantomData,
     slice, str,
+    sync::RwLock,
 };
 
+static STRING_CACHE: Lazy<RwLock<HashMap<String, CString>>> = Lazy::new(|| Default::default());
+
 // https://mlir.llvm.org/docs/CAPI/#stringref
+//
+// TODO The documentation says string refs do not have to be null-terminated.
+// But it looks like some APIs do not handle strings not null-terminated?
 pub struct StringRef<'a> {
     string: MlirStringRef,
     _parent: PhantomData<&'a ()>,
 }
 
-// TODO Handle non-null terminated strings.
 impl<'a> StringRef<'a> {
     pub fn as_str(&self) -> &CStr {
         unsafe {
@@ -37,7 +44,15 @@ impl<'a> StringRef<'a> {
 
 impl From<&str> for StringRef<'_> {
     fn from(string: &str) -> Self {
-        let string = CString::new(string).unwrap();
+        if !STRING_CACHE.read().unwrap().contains_key(string) {
+            STRING_CACHE
+                .write()
+                .unwrap()
+                .insert(string.to_owned(), CString::new(string).unwrap());
+        }
+
+        let lock = STRING_CACHE.read().unwrap();
+        let string = lock.get(string).unwrap();
 
         unsafe { Self::from_raw(mlirStringRefCreateFromCString(string.as_ptr())) }
     }
--- src/utility.rs
+++ src/utility.rs
@@ -1,16 +1,10 @@
+use crate::string_ref::StringRef;
 use mlir_sys::MlirStringRef;
-use std::ffi::CString;
 use std::mem::forget;
 use std::ptr::null_mut;
 
-// TODO Is this safe?
 pub(crate) unsafe fn as_string_ref(string: &str) -> MlirStringRef {
-    let length = string.len() as u64;
-
-    MlirStringRef {
-        data: CString::new(string).unwrap().into_raw(),
-        length,
-    }
+    StringRef::from(string).to_raw()
 }
 
 pub(crate) unsafe fn into_raw_array<T>(mut xs: Vec<T>) -> *mut T {
