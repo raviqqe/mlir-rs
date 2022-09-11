28ae2c7c79b258002c9dad5b0716d216c9d51df0
Yota Toyama <raviqqe@gmail.com>
2022-09-11 19:44:12 +0000

    Refactor

--- src/utility.rs
+++ src/utility.rs
@@ -1,14 +1,4 @@
-use std::mem::forget;
-use std::ptr::null_mut;
-
-pub(crate) unsafe fn into_raw_array<T>(mut xs: Vec<T>) -> *mut T {
-    if xs.is_empty() {
-        null_mut()
-    } else {
-        let ptr = xs.as_mut_ptr();
-
-        forget(xs);
-
-        ptr
-    }
+// TODO Use into_raw_parts.
+pub(crate) unsafe fn into_raw_array<T>(xs: Vec<T>) -> *mut T {
+    xs.leak().as_mut_ptr()
 }
