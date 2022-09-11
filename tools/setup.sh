e150eb845d8a24c7f343b6cbee13d4f0b251bd91
Yota Toyama <raviqqe@gmail.com>
2022-09-11 20:27:17 +0000

    Set up PATH

--- tools/setup.sh
+++ tools/setup.sh
@@ -10,6 +10,7 @@ set -e
 
 curl -fsSL https://apt.llvm.org/llvm.sh | sudo bash -s 15 all
 sudo apt -y install libmlir-15-dev mlir-15-tools
+echo PATH=/usr/lib/llvm-15/bin:$PATH >>$GITHUB_ENV
 
 if [ -n "$GITHUB_ENV" ]; then
   echo PATH=$(brew --prefix)/opt/llvm@$llvm_version/bin:$PATH >>$GITHUB_ENV
