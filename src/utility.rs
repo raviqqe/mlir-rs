0e8b8a1c24790ecc2d1884a54460a5d59adc10a4
Yota Toyama <raviqqe@gmail.com>
2022-09-13 05:55:56 +0000

    Add comment

--- src/utility.rs
+++ src/utility.rs
@@ -1,10 +1,12 @@
 use crate::{context::Context, dialect_registry::DialectRegistry};
 use mlir_sys::{mlirRegisterAllDialects, mlirRegisterAllLLVMTranslations};
 
+/// Registers all dialects to a dialect registry.
 pub fn register_all_dialects(registry: &DialectRegistry) {
     unsafe { mlirRegisterAllDialects(registry.to_raw()) }
 }
 
+/// Register all translations from other dialects to the `llvm` dialect.
 pub fn register_all_llvm_translations(context: &Context) {
     unsafe { mlirRegisterAllLLVMTranslations(context.to_raw()) }
 }
