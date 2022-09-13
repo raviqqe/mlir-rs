f0b2af975b0cf6848262c871bc88c9e8c2ce0c7c
Yota Toyama <raviqqe@gmail.com>
2022-09-13 03:09:50 +0000

    Document pass

--- src/pass.rs
+++ src/pass.rs
@@ -2,15 +2,18 @@ use mlir_sys::{
     mlirCreateConversionConvertArithmeticToLLVM, mlirCreateConversionConvertFuncToLLVM, MlirPass,
 };
 
+/// A pass.
 pub struct Pass {
     raw: MlirPass,
 }
 
 impl Pass {
+    /// Creates a pass to convert the `arith` dialect to the `llvm` dialect.
     pub fn convert_arithmetic_to_llvm() -> Self {
         Self::from_raw_fn(mlirCreateConversionConvertArithmeticToLLVM)
     }
 
+    /// Creates a pass to convert the `func` dialect to the `llvm` dialect.
     pub fn convert_func_to_llvm() -> Self {
         Self::from_raw_fn(mlirCreateConversionConvertFuncToLLVM)
     }
