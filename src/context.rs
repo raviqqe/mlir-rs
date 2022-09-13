eaec71d33e6b6b27800757b3d11d800ca5edc08e
Yota Toyama <raviqqe@gmail.com>
2022-09-13 06:16:37 +0000

    Update documentation

--- src/context.rs
+++ src/context.rs
@@ -6,6 +6,9 @@ use mlir_sys::{
 };
 use std::{marker::PhantomData, mem::ManuallyDrop, ops::Deref};
 
+/// A context of IR, dialects, and passes.
+///
+/// Contexts own various objects, such as types, locations, and dialect instances.
 pub struct Context {
     raw: MlirContext,
 }
