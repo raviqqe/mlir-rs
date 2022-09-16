33ae68b847a48ce5b108e3a54ebabd5c71170ef0
Yota Toyama <raviqqe@gmail.com>
2022-09-16 02:32:14 +0000

    Compare attributes

--- src/attribute.rs
+++ src/attribute.rs
@@ -2,8 +2,15 @@ use crate::{
     context::{Context, ContextRef},
     string_ref::StringRef,
 };
-use mlir_sys::{mlirAttributeDump, mlirAttributeGetContext, mlirAttributeParseGet, MlirAttribute};
-use std::marker::PhantomData;
+use mlir_sys::{
+    mlirAttributeDump, mlirAttributeEqual, mlirAttributeGetContext, mlirAttributeParseGet,
+    mlirAttributePrint, MlirAttribute, MlirStringRef,
+};
+use std::{
+    ffi::c_void,
+    fmt::{self, Display, Formatter},
+    marker::PhantomData,
+};
 
 /// An attribute.
 // Attributes are always values but their internal storage is owned by contexts.
@@ -15,7 +22,7 @@ pub struct Attribute<'c> {
 
 impl<'c> Attribute<'c> {
     /// Parses an attribute.
-    pub fn parse(context: &Context, source: &str) -> Option<Self> {
+    pub fn parse(context: &'c Context, source: &str) -> Option<Self> {
         unsafe {
             Self::from_option_raw(mlirAttributeParseGet(
                 context.to_raw(),
@@ -54,6 +61,35 @@ impl<'c> Attribute<'c> {
     }
 }
 
+impl<'c> PartialEq for Attribute<'c> {
+    fn eq(&self, other: &Self) -> bool {
+        unsafe { mlirAttributeEqual(self.raw, other.raw) }
+    }
+}
+
+impl<'c> Eq for Attribute<'c> {}
+
+impl<'c> Display for Attribute<'c> {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        let mut data = (formatter, Ok(()));
+
+        unsafe extern "C" fn callback(string: MlirStringRef, data: *mut c_void) {
+            let data = &mut *(data as *mut (&mut Formatter, fmt::Result));
+            let result = write!(data.0, "{}", StringRef::from_raw(string).as_str());
+
+            if data.1.is_ok() {
+                data.1 = result;
+            }
+        }
+
+        unsafe {
+            mlirAttributePrint(self.raw, Some(callback), &mut data as *mut _ as *mut c_void);
+        }
+
+        data.1
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -73,5 +109,33 @@ mod tests {
     #[test]
     fn context() {
         Attribute::parse(&Context::new(), "unit").unwrap().context();
+    }
+
+    #[test]
+    fn equal() {
+        let context = Context::new();
+        let attribute = Attribute::parse(&context, "unit").unwrap();
+
+        assert_eq!(attribute, attribute);
+    }
+
+    #[test]
+    fn not_equal() {
+        let context = Context::new();
+
+        assert_ne!(
+            Attribute::parse(&context, "unit").unwrap(),
+            Attribute::parse(&context, "42").unwrap()
+        );
+    }
+
+    #[test]
+    fn display() {
+        assert_eq!(
+            Attribute::parse(&Context::new(), "unit")
+                .unwrap()
+                .to_string(),
+            "unit"
+        );
     }
 }
