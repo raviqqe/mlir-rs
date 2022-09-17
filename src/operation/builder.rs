12cc51a8f7b6fc250009aacd217a1a6d7f758e86
Yota Toyama <raviqqe@gmail.com>
2022-09-17 03:47:48 +0000

    Fix

--- src/operation/builder.rs
+++ src/operation/builder.rs
@@ -172,15 +172,22 @@ mod tests {
     #[test]
     fn enable_result_type_infrence() {
         let context = Context::new();
-        let location = Location::unknown(&context);
+        context.load_all_available_dialects();
 
-        // context.load_all_available_dialects();
-        let block = Block::new(&[(Type::index(&context), location)]);
+        let location = Location::unknown(&context);
+        let r#type = Type::index(&context);
+        let block = Block::new(&[(r#type, location)]);
         let argument = *block.argument(0).unwrap();
 
-        Builder::new("arith.addi", location)
-            .add_operands(&[argument, argument])
-            .enable_result_type_infrence()
-            .build();
+        assert_eq!(
+            Builder::new("arith.addi", location)
+                .add_operands(&[argument, argument])
+                .enable_result_type_infrence()
+                .build()
+                .result(0)
+                .unwrap()
+                .r#type(),
+            r#type,
+        );
     }
 }
