1dc810057c01fd2aaa81a8ef3f3eb2ff3cbdc3aa
Yota Toyama <raviqqe@gmail.com>
2022-09-21 02:47:05 +0000

    Fix

--- src/ir/type/mem_ref.rs
+++ src/ir/type/mem_ref.rs
@@ -1,11 +1,11 @@
 use super::TypeLike;
 use crate::{
-    ir::{affine_map::AffineMap, Attribute, Type},
+    ir::{affine_map::AffineMap, Attribute, Location, Type},
     Error,
 };
 use mlir_sys::{
-    mlirMemRefTypeGet, mlirMemRefTypeGetAffineMap, mlirMemRefTypeGetLayout,
-    mlirMemRefTypeGetMemorySpace, MlirType,
+    mlirMemRefTypeGet, mlirMemRefTypeGetAffineMap, mlirMemRefTypeGetChecked,
+    mlirMemRefTypeGetLayout, mlirMemRefTypeGetMemorySpace, MlirType,
 };
 use std::fmt::{self, Display, Formatter};
 
@@ -23,16 +23,34 @@ impl<'c> MemRef<'c> {
         layout: Attribute<'c>,
         memory_space: Attribute<'c>,
     ) -> Self {
-        Self {
-            r#type: unsafe {
-                Type::from_raw(mlirMemRefTypeGet(
-                    r#type.to_raw(),
-                    dimensions.len() as isize,
-                    dimensions.as_ptr() as *const i64,
-                    layout.to_raw(),
-                    memory_space.to_raw(),
-                ))
-            },
+        unsafe {
+            Self::from_raw(mlirMemRefTypeGet(
+                r#type.to_raw(),
+                dimensions.len() as isize,
+                dimensions.as_ptr() as *const i64,
+                layout.to_raw(),
+                memory_space.to_raw(),
+            ))
+        }
+    }
+
+    /// Creates a mem-ref type with validation.
+    pub fn checked(
+        location: Location<'c>,
+        r#type: Type<'c>,
+        dimensions: &[u64],
+        layout: Attribute<'c>,
+        memory_space: Attribute<'c>,
+    ) -> Option<Self> {
+        unsafe {
+            Self::from_option_raw(mlirMemRefTypeGetChecked(
+                location.to_raw(),
+                r#type.to_raw(),
+                dimensions.len() as isize,
+                dimensions.as_ptr() as *const i64,
+                layout.to_raw(),
+                memory_space.to_raw(),
+            ))
         }
     }
 
@@ -49,6 +67,20 @@ impl<'c> MemRef<'c> {
     /// Gets a memory space.
     pub fn memory_space(&self) -> Attribute<'c> {
         unsafe { Attribute::from_raw(mlirMemRefTypeGetMemorySpace(self.r#type.to_raw())) }
+    }
+
+    unsafe fn from_raw(raw: MlirType) -> Self {
+        Self {
+            r#type: Type::from_raw(raw),
+        }
+    }
+
+    unsafe fn from_option_raw(raw: MlirType) -> Option<Self> {
+        if raw.ptr.is_null() {
+            None
+        } else {
+            Some(Self::from_raw(raw))
+        }
     }
 }
 
