d74c3158319b869f8f19a7d9190d2b533a9b058d
Yota Toyama <raviqqe@gmail.com>
2023-05-08 15:34:37 +1000

    Fix

--- tools/api_completeness.sh
+++ tools/api_completeness.sh
@@ -5,5 +5,17 @@ set -e
 cd $(dirname $0)/..
 cd melior
 
-cargo install cargo-expand
-cargo expand >/tmp/cargo_expand.txt
+count() {
+  sort -u | wc -l
+}
+
+implemented_count() {
+  cargo install cargo-expand
+  cargo expand | grep -o '\(m\|M\)lir[A-Z][a-zA-Z0-9]*' | count
+}
+
+upstream_count() {
+  cat $(find $(brew --prefix llvm)/include/mlir-c -type f) | grep -o '\(m\|M\)lir[A-Z][a-zA-Z0-9]*' | count
+}
+
+echo $(implemented_count) / $(upstream_count) | bc -l
