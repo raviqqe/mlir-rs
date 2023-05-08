2d848dbe569460ac0aaadda1ed3a099e578b5930
Yota Toyama <raviqqe@gmail.com>
2023-05-08 20:43:39 +1000

    Refactor

--- macro/src/conversion_passes.rs
+++ macro/src/conversion_passes.rs
@@ -4,17 +4,15 @@ use proc_macro2::Ident;
 use std::error::Error;
 
 pub fn generate(identifiers: &[Ident]) -> Result<TokenStream, Box<dyn Error>> {
-    passes::generate(identifiers, extract_pass_name)
-}
+    passes::generate(identifiers, |mut name| {
+        if let Some(other) = name.strip_prefix("mlirCreateConversion") {
+            name = other.into();
+        }
 
-fn extract_pass_name(mut name: &str) -> String {
-    if let Some(other) = name.strip_prefix("mlirCreateConversion") {
-        name = other.into();
-    }
+        if let Some(other) = name.strip_suffix("ConversionPass") {
+            name = other.into();
+        }
 
-    if let Some(other) = name.strip_suffix("ConversionPass") {
-        name = other.into();
-    }
-
-    name.into()
+        name.into()
+    })
 }
--- macro/src/transform_passes.rs
+++ macro/src/transform_passes.rs
@@ -4,11 +4,9 @@ use proc_macro2::Ident;
 use std::error::Error;
 
 pub fn generate(identifiers: &[Ident]) -> Result<TokenStream, Box<dyn Error>> {
-    passes::generate(identifiers, extract_pass_name)
-}
-
-fn extract_pass_name(name: &str) -> String {
-    name.strip_prefix("mlirCreateTransforms")
-        .unwrap_or(name)
-        .into()
+    passes::generate(identifiers, |name| {
+        name.strip_prefix("mlirCreateTransforms")
+            .unwrap_or(name)
+            .into()
+    })
 }
