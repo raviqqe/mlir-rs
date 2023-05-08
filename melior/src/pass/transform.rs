d6554edad9164556f02ce5e3c58a8934c351f35d
Yota Toyama <raviqqe@gmail.com>
2023-05-08 20:46:57 +1000

    Fix

--- melior/src/pass/transform.rs
+++ melior/src/pass/transform.rs
@@ -1,3 +1,18 @@
 //! General transformation passes.
 
-melior_macro::transform_passes!();
+melior_macro::transform_passes!(
+    mlirCreateTransformsCSE,
+    mlirCreateTransformsCanonicalizer,
+    mlirCreateTransformsControlFlowSink,
+    mlirCreateTransformsGenerateRuntimeVerification,
+    mlirCreateTransformsInliner,
+    mlirCreateTransformsLocationSnapshot,
+    mlirCreateTransformsLoopInvariantCodeMotion,
+    mlirCreateTransformsPrintOpStats,
+    mlirCreateTransformsSCCP,
+    mlirCreateTransformsStripDebugInfo,
+    mlirCreateTransformsSymbolDCE,
+    mlirCreateTransformsSymbolPrivatize,
+    mlirCreateTransformsTopologicalSort,
+    mlirCreateTransformsViewOpGraph,
+);
