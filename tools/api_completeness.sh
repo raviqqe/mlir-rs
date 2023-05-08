2cfe85b59a48715f0b7cc858a377629012846cc6
Yota Toyama <raviqqe@gmail.com>
2023-05-08 15:35:31 +1000

    Refactor

--- tools/api_completeness.sh
+++ tools/api_completeness.sh
@@ -6,16 +6,16 @@ cd $(dirname $0)/..
 cd melior
 
 count() {
-  sort -u | wc -l
+  grep -o '\(m\|M\)lir[A-Z][a-zA-Z0-9]*' | sort -u | wc -l
 }
 
 implemented_count() {
   cargo install cargo-expand
-  cargo expand | grep -o '\(m\|M\)lir[A-Z][a-zA-Z0-9]*' | count
+  cargo expand | count
 }
 
 upstream_count() {
-  cat $(find $(brew --prefix llvm)/include/mlir-c -type f) | grep -o '\(m\|M\)lir[A-Z][a-zA-Z0-9]*' | count
+  cat $(find $(brew --prefix llvm)/include/mlir-c -type f) | count
 }
 
 echo $(implemented_count) / $(upstream_count) | bc -l
