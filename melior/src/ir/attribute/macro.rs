42fc1ad3eb5259e27875ee96b2debad7f35ff6cd
Yota Toyama <raviqqe@gmail.com>
2023-05-10 15:10:56 +1000

    Fix

--- melior/src/ir/attribute/macro.rs
+++ melior/src/ir/attribute/macro.rs
@@ -1,12 +1,17 @@
 macro_rules! attribute_traits {
     ($name: ident) => {
-        impl<'c> Display for FloatAttribute<'c> {
+        use std::{
+            convert::TryFrom,
+            fmt::{self, Debug, Display, Formatter},
+        };
+
+        impl<'c> Display for $name<'c> {
             fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
                 Display::fmt(&self.attribute, formatter)
             }
         }
 
-        impl<'c> Debug for FloatAttribute<'c> {
+        impl<'c> Debug for $name<'c> {
             fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
                 Display::fmt(self, formatter)
             }
