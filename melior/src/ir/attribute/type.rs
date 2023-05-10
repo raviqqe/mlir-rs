cdd31b488398411adaeab8c491a716b3db706bbf
Yota Toyama <raviqqe@gmail.com>
2023-05-10 15:53:03 +1000

    Add method

--- melior/src/ir/attribute/type.rs
+++ melior/src/ir/attribute/type.rs
@@ -3,7 +3,7 @@ use crate::{
     ir::{Type, TypeLike},
     Error,
 };
-use mlir_sys::{mlirTypeAttrGet, MlirAttribute};
+use mlir_sys::{mlirTypeAttrGet, mlirTypeAttrGetValue, MlirAttribute};
 
 /// A type attribute.
 #[derive(Clone, Copy)]
@@ -16,6 +16,26 @@ impl<'c> TypeAttribute<'c> {
     pub fn new(r#type: Type<'c>) -> Self {
         unsafe { Self::from_raw(mlirTypeAttrGet(r#type.to_raw())) }
     }
+
+    /// gets a type value.
+    pub fn value(&self) -> Type<'c> {
+        unsafe { Type::from_raw(mlirTypeAttrGetValue(self.to_raw())) }
+    }
 }
 
 attribute_traits!(TypeAttribute, is_type, "type");
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::Context;
+
+    #[test]
+    fn value() {
+        let context = Context::new();
+        let r#type = Type::index(&context);
+        let attribute = TypeAttribute::new(r#type);
+
+        assert_eq!(attribute.value(), r#type);
+    }
+}
