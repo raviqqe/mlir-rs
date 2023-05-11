0610ea622d39f92ed17f6756a836da3f26e6194a
Yota Toyama <raviqqe@gmail.com>
2023-05-11 13:55:09 +1000

    Fix

--- melior/src/dialect/func.rs
+++ melior/src/dialect/func.rs
@@ -52,7 +52,42 @@ mod tests {
     };
 
     #[test]
-    fn run_on_function_in_nested_module() {
+    fn compile_call() {
+        let context = Context::new();
+        load_all_dialects(&context);
+
+        let location = Location::unknown(&context);
+        let module = Module::new(location);
+
+        let integer_type = Type::index(&context);
+
+        let function = {
+            let block = Block::new(&[(integer_type, location)]);
+
+            block.append_operation(r#return(&[block.argument(0).unwrap().into()], location));
+
+            let region = Region::new();
+            region.append_block(block);
+
+            func(
+                &context,
+                StringAttribute::new(&context, "add"),
+                TypeAttribute::new(
+                    FunctionType::new(&context, &[integer_type], &[integer_type]).into(),
+                ),
+                region,
+                Location::unknown(&context),
+            )
+        };
+
+        module.body().append_operation(function);
+
+        assert!(module.as_operation().verify());
+        insta::assert_display_snapshot!(module.as_operation());
+    }
+
+    #[test]
+    fn compile_function() {
         let context = Context::new();
         load_all_dialects(&context);
 
