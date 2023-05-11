600256e03a50f4f9d3cce20a30cddd4e06902db0
Yota Toyama <raviqqe@gmail.com>
2023-05-11 14:05:01 +1000

    Fix

--- melior/src/ir/attribute/flat_symbol_ref.rs
+++ melior/src/ir/attribute/flat_symbol_ref.rs
@@ -1,6 +1,6 @@
 use super::{Attribute, AttributeLike};
-use crate::Error;
-use mlir_sys::{mlirFlatSymbolRefAttrGet, MlirAttribute};
+use crate::{Context, Error, StringRef};
+use mlir_sys::{mlirFlatSymbolRefAttrGet, mlirFlatSymbolRefAttrGetValue, MlirAttribute};
 
 /// A flat symbol ref attribute.
 #[derive(Clone, Copy)]
@@ -11,7 +11,18 @@ pub struct FlatSymbolRefAttribute<'c> {
 impl<'c> FlatSymbolRefAttribute<'c> {
     /// Creates an flat symbol ref attribute.
     pub fn new(context: &'c Context, symbol: &str) -> Self {
-        unsafe { Self::from_raw(mlirFlatSymbolRefAttrGet(r#type.to_raw(), integer)) }
+        unsafe {
+            Self::from_raw(mlirFlatSymbolRefAttrGet(
+                context.to_raw(),
+                StringRef::from(symbol).to_raw(),
+            ))
+        }
+    }
+
+    pub fn value(&self) -> &str {
+        unsafe { StringRef::from_raw(mlirFlatSymbolRefAttrGetValue(self.to_raw())) }
+            .as_str()
+            .unwrap()
     }
 }
 
