255992666f908c20ee2f2095dcb40145f03da17a
Yota Toyama <raviqqe@gmail.com>
2023-05-21 17:50:01 +1000

    Fix

--- melior/src/ir/operation/printing_flags.rs
+++ melior/src/ir/operation/printing_flags.rs
@@ -1,1 +1,10 @@
+use mlir_sys::{mlirOpPrintingFlagsCreate, MlirOpPrintingFlags};
+
+#[derive(Clone, Copy, Debug)]
 pub struct OperationPrintingFlags(MlirOpPrintingFlags);
+
+impl OperationPrintingFlags {
+    pub fn new() -> Self {
+        Self(unsafe { mlirOpPrintingFlagsCreate() })
+    }
+}
