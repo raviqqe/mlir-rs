4be6f8b478da4aa52146fecb4bd4ad4fd21abe56
Yota Toyama <raviqqe@gmail.com>
2023-05-26 16:47:44 +1000

    Add insert value

--- melior/src/dialect/llvm.rs
+++ melior/src/dialect/llvm.rs
@@ -1,3 +1,83 @@
 //! `llvm` dialect.
 
+use crate::{
+    ir::{
+        attribute::StringAttribute, operation::OperationBuilder, Identifier, Location, Operation,
+        Value,
+    },
+    Context,
+};
+
 pub mod r#type;
+
+/// Creates a `cf.assert` operation.
+pub fn insert_value<'c>(
+    context: &'c Context,
+    argument: Value<'c>,
+    message: &str,
+    location: Location<'c>,
+) -> Operation<'c> {
+    OperationBuilder::new("cf.assert", location)
+        .add_attributes(&[(
+            Identifier::new(context, "msg"),
+            StringAttribute::new(context, message).into(),
+        )])
+        .add_operands(&[argument])
+        .build()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        dialect::{arith, func},
+        ir::{
+            attribute::{IntegerAttribute, StringAttribute, TypeAttribute},
+            r#type::{FunctionType, IntegerType, Type},
+            Block, Module, Region,
+        },
+        test::load_all_dialects,
+        Context,
+    };
+
+    #[test]
+    fn compile_assert() {
+        let context = Context::new();
+        load_all_dialects(&context);
+
+        let location = Location::unknown(&context);
+        let module = Module::new(location);
+        let bool_type: Type = IntegerType::new(&context, 1).into();
+
+        module.body().append_operation(func::func(
+            &context,
+            StringAttribute::new(&context, "foo"),
+            TypeAttribute::new(FunctionType::new(&context, &[], &[]).into()),
+            {
+                let block = Block::new(&[]);
+                let operand = block
+                    .append_operation(arith::constant(
+                        &context,
+                        IntegerAttribute::new(1, bool_type).into(),
+                        location,
+                    ))
+                    .result(0)
+                    .unwrap()
+                    .into();
+
+                block.append_operation(assert(&context, operand, "assert message", location));
+
+                block.append_operation(func::r#return(&[], location));
+
+                let region = Region::new();
+                region.append_block(block);
+                region
+            },
+            &[],
+            location,
+        ));
+
+        assert!(module.as_operation().verify());
+        insta::assert_display_snapshot!(module.as_operation());
+    }
+}
