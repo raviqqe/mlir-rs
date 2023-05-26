bb2fd5ecc5b6921fba94f3cfcd40ac5a6c5042a2
Yota Toyama <raviqqe@gmail.com>
2023-05-26 17:13:08 +1000

    Convert

--- melior/src/dialect/llvm.rs
+++ melior/src/dialect/llvm.rs
@@ -35,15 +35,35 @@ mod tests {
             r#type::FunctionType,
             Block, Module, Region, Type,
         },
+        pass::{self, PassManager},
         test::create_test_context,
     };
+
+    fn convert_module<'c>(context: &'c Context, module: &mut Module<'c>) {
+        let pass_manager = PassManager::new(&context);
+
+        pass_manager.add_pass(pass::conversion::create_func_to_llvm());
+        pass_manager
+            .nested_under("func.func")
+            .add_pass(pass::conversion::create_arith_to_llvm());
+        pass_manager
+            .nested_under("func.func")
+            .add_pass(pass::conversion::create_index_to_llvm_pass());
+        pass_manager.add_pass(pass::conversion::create_scf_to_control_flow());
+        pass_manager.add_pass(pass::conversion::create_control_flow_to_llvm());
+        pass_manager.add_pass(pass::conversion::create_mem_ref_to_llvm());
+
+        module.as_operation().dump();
+        assert_eq!(pass_manager.run(module), Ok(()));
+        assert!(module.as_operation().verify());
+    }
 
     #[test]
     fn compile_insert_value() {
         let context = create_test_context();
 
         let location = Location::unknown(&context);
-        let module = Module::new(location);
+        let mut module = Module::new(location);
         let index_type = Type::index(&context);
         let struct_type = r#type::r#struct(&context, &[index_type], false).into();
 
@@ -80,6 +100,8 @@ mod tests {
             &[],
             location,
         ));
+
+        convert_module(&context, &mut module);
 
         assert!(module.as_operation().verify());
         insta::assert_display_snapshot!(module.as_operation());
