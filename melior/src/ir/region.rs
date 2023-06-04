f2ed59434edfac6a007f4b0d9c6870d6239797ce
Yota Toyama <raviqqe@gmail.com>
2023-06-04 19:50:42 +1000

    Fix

--- melior/src/ir/region.rs
+++ melior/src/ir/region.rs
@@ -40,7 +40,7 @@ impl<'c> Region<'c> {
     }
 
     /// Inserts a block after another block.
-    pub fn insert_block_after(&self, one: BlockRef, other: Block) -> BlockRef {
+    pub fn insert_block_after(&self, one: BlockRef<'c, '_>, other: Block<'c>) -> BlockRef {
         unsafe {
             let r#ref = BlockRef::from_raw(other.to_raw());
 
@@ -82,34 +82,34 @@ impl<'c> Region<'c> {
     }
 }
 
-impl Default for Region {
+impl<'c> Default for Region<'c> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl Drop for Region {
+impl<'c> Drop for Region<'c> {
     fn drop(&mut self) {
         unsafe { mlirRegionDestroy(self.raw) }
     }
 }
 
-impl PartialEq for Region {
+impl<'c> PartialEq for Region<'c> {
     fn eq(&self, other: &Self) -> bool {
         unsafe { mlirRegionEqual(self.raw, other.raw) }
     }
 }
 
-impl Eq for Region {}
+impl<'c> Eq for Region<'c> {}
 
 /// A reference to a region.
 #[derive(Clone, Copy, Debug)]
-pub struct RegionRef<'a> {
+pub struct RegionRef<'c, 'a> {
     raw: MlirRegion,
-    _region: PhantomData<&'a Region>,
+    _region: PhantomData<&'a Region<'c>>,
 }
 
-impl<'a> RegionRef<'a> {
+impl<'c, 'a> RegionRef<'c, 'a> {
     /// Creates a region from a raw object.
     ///
     /// # Safety
@@ -136,21 +136,21 @@ impl<'a> RegionRef<'a> {
     }
 }
 
-impl<'a> Deref for RegionRef<'a> {
-    type Target = Region;
+impl<'c, 'a> Deref for RegionRef<'c, 'a> {
+    type Target = Region<'c>;
 
     fn deref(&self) -> &Self::Target {
         unsafe { transmute(self) }
     }
 }
 
-impl<'a> PartialEq for RegionRef<'a> {
+impl<'c, 'a> PartialEq for RegionRef<'c, 'a> {
     fn eq(&self, other: &Self) -> bool {
         unsafe { mlirRegionEqual(self.raw, other.raw) }
     }
 }
 
-impl<'a> Eq for RegionRef<'a> {}
+impl<'c, 'a> Eq for RegionRef<'c, 'a> {}
 
 #[cfg(test)]
 mod tests {
