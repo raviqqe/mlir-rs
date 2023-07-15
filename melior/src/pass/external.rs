45d5b634bd4bc90e9a20987e7635e983f08c8f92
Daan Vanoverloop <daan@vanoverloop.xyz>
2023-07-15 19:14:36 +0200

    Move create_external out of ExternalPass to simplify trait

--- melior/src/pass/external.rs
+++ melior/src/pass/external.rs
@@ -47,46 +47,43 @@ unsafe extern "C" fn callback_clone<'a, T: ExternalPass<'a>>(pass: *mut T) -> *m
     ))
 }
 
-pub trait ExternalPass<'a>: Sized + Clone {
-    fn create_external(self) -> Pass {
-        unsafe {
-            let dep_dialects = self.dependent_dialects();
-            let mut dep_dialects_raw: Vec<_> =
-                dep_dialects.into_iter().map(|d| d.to_raw()).collect();
-            let callbacks = mlir_sys::MlirExternalPassCallbacks {
-                construct: Some(std::mem::transmute(callback_construct::<Self> as *const ())),
-                destruct: Some(std::mem::transmute(callback_destruct::<Self> as *const ())),
-                initialize: Some(std::mem::transmute(
-                    callback_initialize::<Self> as *const (),
-                )),
-                run: Some(std::mem::transmute(callback_run::<Self> as *const ())),
-                clone: Some(std::mem::transmute(callback_clone::<Self> as *const ())),
-            };
-            let pass = Box::<Self>::into_raw(Box::new(self));
-            let pass_ref = pass.as_ref().expect("pass is still valid");
-            let raw_pass = mlir_sys::mlirCreateExternalPass(
-                TypeId::create().to_raw(),
-                pass_ref.name().to_raw(),
-                pass_ref.argument().to_raw(),
-                pass_ref.description().to_raw(),
-                pass_ref.op_name().to_raw(),
-                dep_dialects_raw.len() as isize,
-                dep_dialects_raw.as_mut_ptr(),
-                callbacks,
-                pass as *mut std::ffi::c_void,
-            );
-            Pass::from_raw(raw_pass)
-        }
-    }
-
-    fn name(&self) -> StringRef<'a>;
-    fn argument(&self) -> StringRef<'a>;
-    fn description(&self) -> StringRef<'a>;
-    fn op_name(&self) -> StringRef<'a>;
-    fn dependent_dialects(&self) -> Vec<DialectHandle>;
-
+pub trait ExternalPass<'c>: Sized + Clone {
     fn construct(&mut self) {}
     fn destruct(&mut self) {}
-    fn initialize(&mut self, context: ContextRef<'a>);
+    fn initialize(&mut self, context: ContextRef<'c>);
     fn run(&mut self, operation: OperationRef);
+}
+
+pub fn create_external<'c, T: ExternalPass<'c>>(
+    pass: T,
+    name: &str,
+    argument: &str,
+    description: &str,
+    op_name: &str,
+    dependent_dialects: &[DialectHandle],
+) -> Pass {
+    unsafe {
+        let mut dep_dialects_raw: Vec<_> =
+            dependent_dialects.into_iter().map(|d| d.to_raw()).collect();
+        let callbacks = mlir_sys::MlirExternalPassCallbacks {
+            construct: Some(std::mem::transmute(callback_construct::<T> as *const ())),
+            destruct: Some(std::mem::transmute(callback_destruct::<T> as *const ())),
+            initialize: Some(std::mem::transmute(callback_initialize::<T> as *const ())),
+            run: Some(std::mem::transmute(callback_run::<T> as *const ())),
+            clone: Some(std::mem::transmute(callback_clone::<T> as *const ())),
+        };
+        let pass_box = Box::<T>::into_raw(Box::new(pass));
+        let raw_pass = mlir_sys::mlirCreateExternalPass(
+            TypeId::create().to_raw(),
+            StringRef::from(name).to_raw(),
+            StringRef::from(argument).to_raw(),
+            StringRef::from(description).to_raw(),
+            StringRef::from(op_name).to_raw(),
+            dep_dialects_raw.len() as isize,
+            dep_dialects_raw.as_mut_ptr(),
+            callbacks,
+            pass_box as *mut std::ffi::c_void,
+        );
+        Pass::from_raw(raw_pass)
+    }
 }
