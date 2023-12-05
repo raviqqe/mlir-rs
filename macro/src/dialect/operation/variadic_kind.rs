29565b2e8d88c203fb47dd1ce4c7a3a7c3151d08
Yota Toyama <raviqqe@gmail.com>
2023-12-05 23:02:04 +0900

    Refactor

--- macro/src/dialect/operation/variadic_kind.rs
+++ macro/src/dialect/operation/variadic_kind.rs
@@ -14,17 +14,17 @@ pub enum VariadicKind {
 impl VariadicKind {
     pub fn new(variable_length_count: usize, same_size: bool, attribute_sized: bool) -> Self {
         if variable_length_count <= 1 {
-            VariadicKind::Simple {
+            Self::Simple {
                 variable_length_seen: false,
             }
         } else if same_size {
-            VariadicKind::SameSize {
+            Self::SameSize {
                 variable_length_count,
                 preceding_simple_count: 0,
                 preceding_variadic_count: 0,
             }
         } else if attribute_sized {
-            VariadicKind::AttributeSized {}
+            Self::AttributeSized {}
         } else {
             unimplemented!()
         }
