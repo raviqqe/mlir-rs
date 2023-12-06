b9b55e51e87f3b5c546bbdf069ddde82a3b95378
Yota Toyama <raviqqe@gmail.com>
2023-12-06 09:37:47 +0900

    Move

--- macro/src/dialect/operation.rs
+++ macro/src/dialect/operation.rs
@@ -23,6 +23,66 @@ use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
 use tblgen::{error::WithLocation, record::Record};
 
+pub fn generate_operation(operation: &Operation) -> Result<TokenStream, Error> {
+    let summary = operation.summary()?;
+    let description = operation.description()?;
+    let class_name = format_ident!("{}", &operation.class_name()?);
+    let name = &operation.full_name()?;
+    let accessors = operation
+        .fields()
+        .map(|field| field.accessors())
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let builder = OperationBuilder::new(operation)?;
+    let builder_tokens = generate_operation_builder(&builder)?;
+    let builder_fn = builder.create_op_builder_fn()?;
+    let default_constructor = builder.create_default_constructor()?;
+
+    Ok(quote! {
+        #[doc = #summary]
+        #[doc = "\n\n"]
+        #[doc = #description]
+        pub struct #class_name<'c> {
+            operation: ::melior::ir::operation::Operation<'c>,
+        }
+
+        impl<'c> #class_name<'c> {
+            pub fn name() -> &'static str {
+                #name
+            }
+
+            pub fn operation(&self) -> &::melior::ir::operation::Operation<'c> {
+                &self.operation
+            }
+
+            #builder_fn
+
+            #(#accessors)*
+        }
+
+        #builder_tokens
+
+        #default_constructor
+
+        impl<'c> TryFrom<::melior::ir::operation::Operation<'c>> for #class_name<'c> {
+            type Error = ::melior::Error;
+
+            fn try_from(
+                operation: ::melior::ir::operation::Operation<'c>,
+            ) -> Result<Self, Self::Error> {
+                // TODO Check an operation name.
+                Ok(Self { operation })
+            }
+        }
+
+        impl<'c> From<#class_name<'c>> for ::melior::ir::operation::Operation<'c> {
+            fn from(operation: #class_name<'c>) -> ::melior::ir::operation::Operation<'c> {
+                operation.operation
+            }
+        }
+    })
+}
+
 #[derive(Debug)]
 pub struct Operation<'a> {
     definition: Record<'a>,
@@ -348,64 +408,4 @@ impl<'a> Operation<'a> {
             })
             .collect()
     }
-}
-
-pub fn generate_operation(operation: &Operation) -> Result<TokenStream, Error> {
-    let summary = operation.summary()?;
-    let description = operation.description()?;
-    let class_name = format_ident!("{}", &operation.class_name()?);
-    let name = &operation.full_name()?;
-    let accessors = operation
-        .fields()
-        .map(|field| field.accessors())
-        .collect::<Result<Vec<_>, _>>()?;
-
-    let builder = OperationBuilder::new(operation)?;
-    let builder_tokens = generate_operation_builder(&builder)?;
-    let builder_fn = builder.create_op_builder_fn()?;
-    let default_constructor = builder.create_default_constructor()?;
-
-    Ok(quote! {
-        #[doc = #summary]
-        #[doc = "\n\n"]
-        #[doc = #description]
-        pub struct #class_name<'c> {
-            operation: ::melior::ir::operation::Operation<'c>,
-        }
-
-        impl<'c> #class_name<'c> {
-            pub fn name() -> &'static str {
-                #name
-            }
-
-            pub fn operation(&self) -> &::melior::ir::operation::Operation<'c> {
-                &self.operation
-            }
-
-            #builder_fn
-
-            #(#accessors)*
-        }
-
-        #builder_tokens
-
-        #default_constructor
-
-        impl<'c> TryFrom<::melior::ir::operation::Operation<'c>> for #class_name<'c> {
-            type Error = ::melior::Error;
-
-            fn try_from(
-                operation: ::melior::ir::operation::Operation<'c>,
-            ) -> Result<Self, Self::Error> {
-                // TODO Check an operation name.
-                Ok(Self { operation })
-            }
-        }
-
-        impl<'c> From<#class_name<'c>> for ::melior::ir::operation::Operation<'c> {
-            fn from(operation: #class_name<'c>) -> ::melior::ir::operation::Operation<'c> {
-                operation.operation
-            }
-        }
-    })
 }
