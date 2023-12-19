bb3348c48d4e7832fbc9c7cc032c15d75eea7b52
Esteve Soler Arderiu <esteve.soler@fratilia.com>
2023-12-19 12:58:21 +0100

    Implement `ExecutionEngine::lookup()` proxy.

--- melior/src/execution_engine.rs
+++ melior/src/execution_engine.rs
@@ -1,7 +1,8 @@
 use crate::{ir::Module, logical_result::LogicalResult, string_ref::StringRef, Error};
 use mlir_sys::{
     mlirExecutionEngineCreate, mlirExecutionEngineDestroy, mlirExecutionEngineDumpToObjectFile,
-    mlirExecutionEngineInvokePacked, mlirExecutionEngineRegisterSymbol, MlirExecutionEngine,
+    mlirExecutionEngineInvokePacked, mlirExecutionEngineLookup, mlirExecutionEngineRegisterSymbol,
+    MlirExecutionEngine,
 };
 
 /// An execution engine.
@@ -32,6 +33,17 @@ impl ExecutionEngine {
                 )
             },
         }
+    }
+
+    /// Searches a symbol in a module and returns a pointer to it.
+    ///
+    /// # Safety
+    ///
+    /// This function modifies memory locations pointed by the `arguments`
+    /// argument. If those pointers are invalid or misaligned, calling this
+    /// function might result in undefined behavior.
+    pub fn lookup(&self, name: &str) -> *mut () {
+        unsafe { mlirExecutionEngineLookup(self.raw, StringRef::new(name).to_raw()) as *mut () }
     }
 
     /// Invokes a function in a module. The `arguments` argument includes
