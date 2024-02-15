3c6fdc66a0d0405174900676aa400ee5d11ce1f6
Yota Toyama <raviqqe@gmail.com>
2024-02-15 22:24:59 +0900

    Fix

--- macro/src/dialect/trait.rs
+++ macro/src/dialect/trait.rs
@@ -1,59 +1,6 @@
 use super::error::{Error, OdsError};
 use tblgen::{error::WithLocation, record::Record};
 
-#[derive(Debug, Clone, Copy)]
-pub struct RegionConstraint<'a>(Record<'a>);
-
-impl<'a> RegionConstraint<'a> {
-    pub fn new(record: Record<'a>) -> Self {
-        Self(record)
-    }
-
-    pub fn is_variadic(&self) -> bool {
-        self.0.subclass_of("VariadicRegion")
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct SuccessorConstraint<'a>(Record<'a>);
-
-impl<'a> SuccessorConstraint<'a> {
-    pub fn new(record: Record<'a>) -> Self {
-        Self(record)
-    }
-
-    pub fn is_variadic(&self) -> bool {
-        self.0.subclass_of("VariadicSuccessor")
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct TypeConstraint<'a>(Record<'a>);
-
-impl<'a> TypeConstraint<'a> {
-    pub fn new(record: Record<'a>) -> Self {
-        Self(record)
-    }
-
-    pub fn is_optional(&self) -> bool {
-        self.0.subclass_of("Optional")
-    }
-
-    pub fn is_variadic(&self) -> bool {
-        self.0.subclass_of("Variadic")
-    }
-
-    // TODO Support variadic-of-variadic.
-    #[allow(unused)]
-    pub fn is_variadic_of_variadic(&self) -> bool {
-        self.0.subclass_of("VariadicOfVariadic")
-    }
-
-    pub fn has_unfixed(&self) -> bool {
-        self.is_variadic() || self.is_optional()
-    }
-}
-
 #[derive(Debug, Clone)]
 enum TraitKind {
     Native {
