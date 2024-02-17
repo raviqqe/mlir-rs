8ceed33b516ae3271d9e5639af565b40d0946437
Yota Toyama <raviqqe@gmail.com>
2024-02-18 01:59:41 +0900

    fix

--- macro/src/dialect/generation/operand_accessor.rs
+++ macro/src/dialect/generation/operand_accessor.rs
@@ -2,9 +2,9 @@ use super::element_accessor::generate_element_getter;
 use crate::dialect::operation::Operand;
 use proc_macro2::{Ident, Span, TokenStream};
 
-pub fn generate_operand_accessor(field: &Operand, index: usize, length: usize) -> TokenStream {
+pub fn generate_operand_accessor(operand: &Operand, index: usize, length: usize) -> TokenStream {
     generate_element_getter(
-        field,
+        operand,
         "operand",
         "operands",
         &Ident::new("OperandNotFound", Span::call_site()),
