0a6e7c691a9e41912a1b233a4a9ceb63545e2d38
Edgar <git@edgarluque.com>
2024-05-02 17:37:50 +0200

    update melior to llvm 18

--- tools/setup.sh
+++ tools/setup.sh
@@ -4,7 +4,7 @@ set -e
 
 llvm_version=17
 
-brew install llvm@$llvm_version
+brew install llvm@$llvm_version z3
 
 echo PATH=$(brew --prefix)/opt/llvm@$llvm_version/bin:$PATH >>$GITHUB_ENV
 echo LIBRARY_PATH=$(brew --prefix)/lib:$LIBRARY_PATH >>$GITHUB_ENV
